1d61859e8274b3a6ee7cbf3651a5268abe4257a0
Jan Hönig <jan.hoenig@fau.de>
2018-01-16 21:55:55 +0100

    changed to SoA, paraview now works

--- gravitation.jl
+++ gravitation.jl
@@ -1,56 +1,57 @@
-mutable struct Point
-	position::Array{Float64,1}
-	velocity::Array{Float64,1}
-	acceleration::Array{Float64,1}
-    mass::Float64
+mutable struct Points
+	position::Array{Float64,2}
+	velocity::Array{Float64,2}
+	acceleration::Array{Float64,2}
+	mass::Array{Float64,1}
+	size::Int
 end
 
-function acceleration_update(points::Array{Point,1}, N)
-	for i = 1:N
-		points[i].acceleration = [0., 0.]
-		for j = 1:N
+function acceleration_update(points::Points)
+	for i = 1:points.size
+		points.acceleration[:,i] = [0., 0.]
+		for j = 1:points.size
 			if i == j
 				continue
 			end
-			r = points[i].position - points[j].position
-			points[i].acceleration += - GravitationalConstant * points[1].mass * points[2].mass / norm(r)^3 * r
+			r = points.position[:,i] - points.position[:,j]
+			points.acceleration[:,i] += - GravitationalConstant * points.mass[i] * points.mass[j] / norm(r)^3 * r
 		end
 	end
 end
 
-function velocity_update(points::Array{Point,1}, N)
-	for i = 1:N
-		points[i].velocity += points[i].acceleration * dt / points[i].mass #(accelration+old_acc)/2
+function velocity_update(points::Points)
+	for i = 1:points.size
+		points.velocity[:,i] += points.acceleration[:,i] * dt / points.mass[i] #(accelration+old_acc)/2
 	end
 end
 
-function position_update(points::Array{Point,1}, N)
-	for i = 1:N
-		points[i].position += points[i].velocity * dt  #+ acceleration*dt*dt/mass
+function position_update(points::Points)
+	for i = 1:points.size
+		points.position[:,i] += points.velocity[:,i] * dt  #+ acceleration*dt*dt/mass
 	end
 end
 
 
 using WriteVTK
 
-pointAmount = 20
-
+N = 20
+dims = 2
 GravitationalConstant = 6.673e-8
-points = [Point([rand();rand()],[0.,0.], [0.,0.], rand()) for n in 1:pointAmount]
+
+points = Points(rand(dims,N), zeros(dims,N), zeros(dims,N), rand(N)*100, N)
 
 dt = 1
 
+
 for i = 1:200
-	acceleration_update(points, pointAmount)
-	velocity_update(points, pointAmount)
-	position_update(points, pointAmount)
-	cells = [MeshCell(VTKCellTypes.VTK_EMPTY_CELL, Int[])]
-	vtkfile = vtk_grid(string("my_vtk_file",i), reshape([points[i].position[j] for i = 1:20 for j = 1:2], 2, 20), cells)
+	acceleration_update(points)
+	velocity_update(points)
+	position_update(points)
+	cells = [MeshCell(VTKCellTypes.VTK_VERTEX, [i]) for i = 1:N]
+	vtkfile = vtk_grid(string("my_vtk_file",i), points.position, cells)
 	#vtk_point_data(vtkfile, reshape([points[i].velocity[j] for i = 1:20 for j = 1:2],2,20), "Velocity")
-	vtk_point_data(vtkfile, ([points[i].velocity[1] for i = 1:20], [points[i].velocity[2] for i = 1:20]), "Velocity")
+	##vtk_point_data(vtkfile, ([points[i].velocity[1] for i = 1:20], [points[i].velocity[2] for i = 1:20]), "Velocity")
 	outfiles = vtk_save(vtkfile)
-
-
 end
 
 
