1773099dcebd40f5b5e0ab4f6393c323f124741b
Jan Hönig <jan.hoenig@fau.de>
2018-01-23 08:31:15 +0100

    added assign_mass

--- gravitation.jl
+++ gravitation.jl
@@ -34,23 +34,43 @@ function position_update(points::Points)
 	end
 end
 
+function assign_mass(potential::Array{Float64,2}, points::Points, dx)
+	for i = 1:points.size
+		potential[ceil(Int, points.position[1,i]/dx),ceil(Int, points.position[2,i]/dx)] = points.mass[i]
+	end
+end
 
 using WriteVTK
 
 N = 20
+grid = 200
 dims = 2
 GravitationalConstant = 6.673e-8
+cells = [MeshCell(VTKCellTypes.VTK_VERTEX, [i]) for i = 1:N]
 
 points = Points(rand(dims,N), zeros(dims,N), zeros(dims,N), rand(N)*100, N)
+potential = -ones(Float64, grid, grid)
 
+dx = 1/grid
 dt = 1
 
+assign_mass(potential, points, dx)
+vtk_write_array("my_vti_file0", potential, "Potential")
+outfile = vtk_grid(string("my_vtk_file",0), points.position*grid, cells) do vtk
+	vtk_cell_data(vtk, points.mass, "Mass")
+	vtk_cell_data(vtk, vcat(points.velocity, zeros(1,N)), "Velocity")
 
+	vtk_point_data(vtk, points.mass, "Pointmass")
+	vtk_point_data(vtk, vcat(points.velocity, zeros(1,N)), "Pointvelocity")
+end
+
+
+
+quit()
 for i = 1:2000
 	acceleration_update(points)
 	velocity_update(points)
 	position_update(points)
-	cells = [MeshCell(VTKCellTypes.VTK_VERTEX, [i]) for i = 1:N]
 	outfile = vtk_grid(string("my_vtk_file",i), points.position, cells) do vtk
 		vtk_cell_data(vtk, points.mass, "Mass")
 		vtk_cell_data(vtk, vcat(points.velocity, zeros(1,N)), "Velocity")
