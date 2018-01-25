b2d55776277fd5b23d84aa2b0f75a66981a03ac1
Jan Hönig <jan.hoenig@fau.de>
2018-01-26 00:02:30 +0100

    first ok version of gauss_seidel

--- gravitation.jl
+++ gravitation.jl
@@ -34,9 +34,17 @@ function position_update(points::Points)
 	end
 end
 
-function assign_mass(potential::Array{Float64,2}, points::Points, dx)
+function assign_mass(density::Array{Float64,2}, points::Points, dx)
 	for i = 1:points.size
-		potential[ceil(Int, points.position[1,i]/dx),ceil(Int, points.position[2,i]/dx)] = points.mass[i]
+		density[ceil(Int, points.position[1,i]/dx),ceil(Int, points.position[2,i]/dx)] = points.mass[i]
+	end
+end
+
+function gauss_seidel(potential::Array{Float64,2}, density::Array{Float64,2})
+	for i = 2:grid-1
+		for j = 2:grid-1
+			potential[i,j] = 1/4 * (4*pi*GravitationalConstant*density[i,j] + potential[i-1,j] + potential[i+1,j] + potential[i,j-1] + potential[i,j+1])
+		end
 	end
 end
 
@@ -49,15 +57,21 @@ GravitationalConstant = 6.673e-8
 cells = [MeshCell(VTKCellTypes.VTK_VERTEX, [i]) for i = 1:N]
 
 points = Points(rand(dims,N), zeros(dims,N), zeros(dims,N), rand(N)*100, N)
-potential = -ones(Float64, grid, grid)
+density = zeros(Float64, grid, grid)
+potential = zeros(Float64, grid, grid)
 
 dx = 1/grid
 dt = 1
 
-assign_mass(potential, points, dx)
+assign_mass(density, points, dx)
+for n=1:1000
+	gauss_seidel(potential, density)
+end
+
 
 #vtk_write_array("my_vti_file0", potential, "Potential")
 outfile = vtk_grid("my_vtr_file0", [i*dx for i=0:grid],[i*dx for i=0:grid]) do vtk
+	vtk_cell_data(vtk, density, "Density")
 	vtk_cell_data(vtk, potential, "Potential")
 end
 
