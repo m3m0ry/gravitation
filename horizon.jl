2581d4f319fcc51511b5b022fc8051c1016b4098
Jan Hönig <jan.hoenig@fau.de>
2018-02-16 13:30:55 +0100

    First successful paraview output

--- horizon.jl
+++ horizon.jl
@@ -1,6 +1,7 @@
 using Requests
 import Requests: get, post, put, delete, options
 using CSV
+using WriteVTK
 
 
 
@@ -10,8 +11,8 @@ query = Dict(
 	"CENTER" => "500@10",
 	"MAKE_EPHEM" => "YES",
 	"TABLE_TYPE" => "VECTORS",
-	"START_TIME" => "2018-02-15",
-	"STOP_TIME" => "2018-03-17",
+	"START_TIME" => "2017-02-15",
+	"STOP_TIME" => "2018-02-15",
 	"STEP_SIZE" => "1d",
 	"OUT_UNITS" => "KM-S",
 	"REF_PLANE" => "ECLIPTIC",
@@ -28,11 +29,18 @@ url = "http://ssd.jpl.nasa.gov/horizons_batch.cgi"
 response = get(url, query = query)
 println(response)
 str = readstring(response)
+print(str)
 
 m = match(r"\$\$SOE\n(?<csv_data>[\s-\S]*)\n\$\$EOE", str)
 data = m[:csv_data]
 data = replace(data, ",\n", "\n")
-df = CSV.read(IOBuffer(data); types=[Float64, String, Float64, Float64, Float64, Float64, Float64, Float64, Float64, Float64, Float64], header=["date1", "date2", "X", "Y", "Z", "VX", "VY", "VZ", "Something", "Something", "Something"])
-df
+df = CSV.read(IOBuffer(data); types=[Float64, String, Float64, Float64, Float64, Float64, Float64, Float64, Float64, Float64, Float64], header=["date1", "date2", "X", "Y", "Z", "VX", "VY", "VZ", "LT", "RG", "RR"], nullable=false)
+
+cells = [MeshCell(VTKCellTypes.VTK_VERTEX, [i]) for i = 1:1]
+for i = 1:size(df)[1]
+	outfile = vtk_grid(string("my_vtu_file",i), [df[:X][i] df[:Y][i] df[:Z][i]]', cells) do vtk
+		vtk_cell_data(vtk, [1], "Mass")
+	end
+end
 
 
