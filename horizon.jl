344c4358b037e592d057da1ab1bd12b284f5f084
Jan Hönig <hrominium@gmail.com>
2018-03-14 21:08:54 +0100

    csv and vtk output of horizon refined

--- horizon.jl
+++ horizon.jl
@@ -2,22 +2,7 @@ using Requests
 import Requests: get, post, put, delete, options
 using CSV
 using DataFrames
-
-
-major_bodies = Dict{String,String}(
-	"10" => "Sun",
-	"199" => "Mercury",
-	"299" => "Venus",
-	#"301" => "Moon",
-	"399" => "Earth",
-	#"401" => "Phobos",
-	#"402" => "Deimos",
-	"499" => "Mars",
-	"599" => "Jupiter",
-	"699" => "Saturn",
-	"799" => "Uranus",
-	"800" => "Neptune")
-
+using WriteVTK
 
 query = Dict{String, String}(
 	"batch" => "l",
@@ -38,10 +23,8 @@ query = Dict{String, String}(
 	"OBJ_DATA" => "NO",
 	"VEC_TABLE" => "3")
 
-url = "http://ssd.jpl.nasa.gov/horizons_batch.cgi"
-
-function horizon_data()
-	for (i, k) in enumerate(keys(major_bodies))
+function horizon_data(bodies::Dict{String, String}; url="http://ssd.jpl.nasa.gov/horizons_batch.cgi")
+	for (i, k) in enumerate(keys(bodies))
 		#download data from horizons
 		query["COMMAND"] = k
 		response = get(url, query = query)
@@ -53,30 +36,45 @@ function horizon_data()
 		data = m[:csv_data]
 		data = replace(data, ",\n", "\n")
 		df = CSV.read(IOBuffer(data); types=Dict(2=>String), header=["date1", "date2", "X", "Y", "Z", "VX", "VY", "VZ", "LT", "RG", "RR"], nullable=false)
-		open("$(major_bodies[k]).csv", "w") do f
-			CSV.write(f, df)
+		CSV.write("$(bodies[k]).csv", df)
+	end
+end
+
+function csv_to_vtu(bodies::Dict{String,String}; file_name="my_vtu_file")
+	vectors = Array{DataFrame}(bodies.count)
+
+	for (i, k) in enumerate(keys(bodies))
+		vectors[i] = CSV.read("$(bodies[k]).csv", nullable=false)
+	end
+
+	cells = WriteVTK.MeshCell[MeshCell(VTKCellTypes.VTK_VERTEX, [i]) for i = 1:length(vectors)]
+	for i = 1:size(vectors[1])[1]
+		points = Array{Float64}(3, length(vectors))
+		for j = 1:length(vectors)
+			points[1,j] = vectors[j][:X][i]
+			points[2,j] = vectors[j][:Y][i]
+			points[3,j] = vectors[j][:Z][i]
+		end
+		outfile = vtk_grid(string(file_name,i), points, cells) do vtk
+			vtk_cell_data(vtk, [1 for i = 1:length(vectors)], "Mass")
 		end
 	end
 end
 
-horizon_data()
 
-using WriteVTK
-vectors = Array{DataFrame}(major_bodies.count)
+major_bodies = Dict{String,String}(
+	"10" => "Sun",
+	"199" => "Mercury",
+	"299" => "Venus",
+	#"301" => "Moon",
+	"399" => "Earth",
+	#"401" => "Phobos",
+	#"402" => "Deimos",
+	"499" => "Mars",
+	"599" => "Jupiter",
+	"699" => "Saturn",
+	"799" => "Uranus",
+	"800" => "Neptune")
 
-for (i, k) in enumerate(keys(major_bodies))
-	vectors[i] = CSV.read("$(major_bodies[k]).csv", nullable=false)
-end
-
-cells = WriteVTK.MeshCell[MeshCell(VTKCellTypes.VTK_VERTEX, [i]) for i = 1:length(vectors)]
-for i = 1:size(vectors[1])[1]
-	points = Array{Float64}(3, length(vectors))
-	for j = 1:length(vectors)
-		points[1,j] = vectors[j][:X][i]
-		points[2,j] = vectors[j][:Y][i]
-		points[3,j] = vectors[j][:Z][i]
-	end
-	outfile = vtk_grid(string("my_vtu_file",i), points, cells) do vtk
-		vtk_cell_data(vtk, [1 for i = 1:length(vectors)], "Mass")
-	end
-end
+horizon_data(major_bodies)
+csv_to_vtu(major_bodies, file_name="major_bodies")
